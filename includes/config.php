3fba01246f655e6fdfe44d9a4ba1379b315333ee
wesbarron <54125356+wesbarron@users.noreply.github.com>
2019-09-07 13:46:00 -0400

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Used to store website configuration information.
+ *
+ * @var string or null
+ */
+function config($key = '')
+{
+    $config = [
+        'name' => 'DIY Accoustic to Electric Hybrid Drum Kit',
+        'pretty_uri' => false,
+        'site_url' => 'https://my-first-project-cpsc-4125.herokuapp.com/',
+        'nav_menu' => [
+            '' => 'Home',
+            'about-me' => 'About Me',
+            'diy-conversion' => 'DIY Conversion'
+        ],
+        'template_path' => 'template',
+        'content_path' => 'content',
+        'version' => 'v3.0',
+    ];
+    return isset($config[$key]) ? $config[$key] : null;
+}
