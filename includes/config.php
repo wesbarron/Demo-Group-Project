156a8e4d13330115cf967b480562b1a704ce01f4
wesbarron <barron_wesley@columbusstate.edu>
2019-09-07 18:12:09 +0000

    created home page.

--- includes/config.php
+++ includes/config.php
@@ -13,8 +13,7 @@ function config($key = '')
         'site_url' => 'https://my-first-project-cpsc-4125.herokuapp.com/',
         'nav_menu' => [
             '' => 'Home',
-            'about-me' => 'About Me',
-            'diy-conversion' => 'DIY Conversion'
+            'about-me' => 'About Me'
         ],
         'template_path' => 'template',
         'content_path' => 'content',
