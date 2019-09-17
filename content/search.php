45e26601fa47c679ba9785930beb87a8f7f662a5
wesbarron <barron_wesley@columbusstate.edu>
2019-09-17 18:41:09 +0000

    Changes to complete project

--- content/search.php
+++ content/search.php
@@ -1,7 +1,7 @@
-<div class="container">
-  <form id="contact" action="search.php" target="_blank" method="get">
+<div class="container">  
+  <form id="contact" action="template.php" target="_blank" method="get">
     <h3>Movie Search</h3>
-
+    
     <fieldset>
       <input name="name" placeholder="Movie Title" type="text" tabindex="1" required autofocus>
     </fieldset>
@@ -9,6 +9,6 @@
       <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
     </fieldset>
   </form>
-
-
+ 
+  
 </div>
