99187104c973f9dd1d9e6b8f4756ef36bde6bb37
wesbarron <barron_wesley@columbusstate.edu>
2019-09-17 20:55:46 +0000

    No clue why no Worky

--- content/index.php
+++ content/index.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Search</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/content/style.css">
 </head>
 <body>
-<div class="container">  
+<div class="container">
   <form id="contact" action="search_result.php" target="_blank" method="get">
     <h3>Movie Search</h3>
-    
+
     <fieldset>
       <input name="name" placeholder="Movie Title" type="text" tabindex="1" required autofocus>
     </fieldset>
@@ -19,8 +19,8 @@
       <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
     </fieldset>
   </form>
- 
-  
+
+
 </div>
 </body>
 </html>
