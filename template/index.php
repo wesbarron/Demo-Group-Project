cf7551ffc6413f3f2b8ff94d7de5cc8829fdd7cf
wesbarron <barron_wesley@columbusstate.edu>
2019-09-17 00:20:04 +0000

    Messing with the search page

--- template/index.php
+++ template/index.php
@@ -1,10 +1,26 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Search</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+  <form id="contact" action="search_result.php" target="_blank" method="get">
+    <h3>Movie Search</h3>
 
-require 'includes/config.php';
-require 'includes/functions.php';
-require 'includes/search_result.php';
+    <fieldset>
+      <input name="name" placeholder="Movie Title" type="text" tabindex="1" required autofocus>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+  </form>
 
-init();
-?>
+
+</div>
+</body>
+</html>
