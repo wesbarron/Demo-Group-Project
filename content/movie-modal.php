e2c62dd1c3b49b3e7d0bda35c3e96d2af7d4ae6b
wesbarron <barron_wesley@columbusstate.edu>
2019-09-28 00:17:18 +0000

    tweeking modal output.

---
+++ content/movie-modal.php
@@ -0,0 +1,26 @@
+'<!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">'.$movie["Title"].'</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+
+        <!-- Modal body -->
+        <div class="modal-body">
+          Year: ' . $movie["Year"] . '<br>
+          imdbID: ' . $movie["imdbID"] . '<br>
+          Type: ' . $movie["Type"] . '<br>
+        </div>
+
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+
+      </div>
+    </div>
+  </div>';
--- content/search_result.php
+++ content/search_result.php
@@ -34,34 +34,6 @@ $output .= "<h3>".$movie['Title']."</h3>";
 $output .= "<li>".$movie['Year']."</li>";
 $output .= "<img src='" . $movie['Poster'] . "' width='250px' height='300px' alt='Comming Soon!'>";
 $output .= "</a><br><br><br>";
-    if ($output == $movie['Title']) {
-$output .= '<!-- The Modal -->
-  <div class="modal" id="myModal">
-    <div class="modal-dialog">
-      <div class="modal-content">
-
-        <!-- Modal Header -->
-        <div class="modal-header">
-          <h4 class="modal-title">'.$movie["Title"].'</h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>
-
-        <!-- Modal body -->
-        <div class="modal-body">
-          Year: ' . $movie["Year"] . '<br>
-          imdbID: ' . $movie["imdbID"] . '<br>
-          Type: ' . $movie["Type"] . '<br>
-        </div>
-
-        <!-- Modal footer -->
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-        </div>
-
-      </div>
-    </div>
-  </div>';
-    }
 }
 $output .= "</ul>";
 
@@ -80,7 +52,7 @@ echo $output;
       $('.movie-click').click(function (e){
           $.ajax({
               type: 'get',
-              url: "search_result.php",
+              url: "movie_modal.php",
               data: {data : true},
               dataType:"HTML",
               success: function(data)
