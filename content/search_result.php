4ddba1c006b96c00ffea58ad64f92f2df6c356b6
wesbarron <barron_wesley@columbusstate.edu>
2019-09-29 20:13:54 +0000

    messing with modal code.

--- content/search_result.php
+++ content/search_result.php
@@ -41,7 +41,9 @@ $output .= "</ul>";
 echo $output;
 ?>
 
+<div id="my-content">
 
+</div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -58,7 +60,7 @@ echo $output;
               dataType:"HTML",
               success: function(data)
               {
-                  $('#myModal').html(data) // The server's response is now placed inside your target div
+                  $('#my-content').html(data) // The server's response is now placed inside your target div
               },
               error: function()
               {
