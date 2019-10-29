fa14ea4de43eb0d36bccdb79024582194f1e0681
wesbarron <barron_wesley@columbusstate.edu>
2019-10-29 00:56:37 +0000

    adding function

--- content/index.php
+++ content/index.php
@@ -25,7 +25,40 @@
 <div>
 <?php
 
- getComic();
+$url = "http://www.omdbapi.com/?s=";
+$search_key = $_POST["name"];
+strtolower($search_key);
+$search_key = str_replace(" ", "+", $search_key);
+$api_key = "&apikey=d42aca4a";
+$search_url = $url . $search_key . $api_key;
+
+
+$handle = curl_init();
+curl_setopt($handle, CURLOPT_URL, $search_url);
+curl_setopt_array($handle,
+array(
+    CURLOPT_URL => $search_url,
+    CURLOPT_RETURNTRANSFER => true
+)
+);
+$output = curl_exec($handle);
+$response = json_decode($output, true);
+curl_close($handle);
+
+
+$output = "<div>";
+$output .= "<ul>";
+
+foreach ($response['Search'] as $movie) {
+$output .= "<h3>".$movie['Title']."</h3>";
+$output .= "<li>".$movie['Year']."</li>";
+$output .= "<li class='imdb-ID'>".$movie['imdbID']."</li>";
+$output .= "<img src='" . $movie['Poster'] . "' width='250px' height='300px' alt='Comming Soon!'>";
+}
+$output .= "</ul>";
+$output .= "</div>";
+
+echo $output;
 
 ?>
 </div>
